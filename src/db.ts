08cf612030ecd375dad707c899e4904b0bc6b999
ksbalawat <ksbalawat2002@gmail.com>
2025-03-23 22:26:12 +0530

    Updated db.ts

--- src/db.ts
+++ src/db.ts
@@ -11,6 +11,14 @@ const UserSchema = new Schema({
 
 export const UserModel = model("User", UserSchema);
 
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags:[{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    userId:[{type: mongoose.Types.ObjectId, ref: 'User', required: true}],
+})
+
+export const ContentModel = model("Content", ContentSchema);
 
 // const userSchema = new Schema({
 //     username: {
